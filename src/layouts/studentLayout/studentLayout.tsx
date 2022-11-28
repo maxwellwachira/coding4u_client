721d5b0c9483b559425e7ebf119140bc409e4659
maxwellwachira <maxwellwachira67@gmail.com>
2022-11-28 17:19:44 +0300

    student dashboard layout

---
+++ src/layouts/studentLayout/studentLayout.tsx
@@ -0,0 +1,106 @@
+import { useState, ReactNode } from 'react';
+import { Burger, Header, Navbar, MediaQuery, ScrollArea, Text, AppShell, Box } from '@mantine/core';
+import {
+  IconGauge,
+  IconSchool,
+  IconUsers,
+  IconCertificate,
+  IconLogout,
+} from '@tabler/icons';
+import { useRouter } from 'next/router';
+
+import { UserButton } from '../../components/userButton/userButton';
+import { LinksGroup } from '../../components/navbarLinkGroups/navbarLinkGroups';
+import { useStyles } from './studentLayout.styles';
+import { colors } from '../../constants/colors';
+import { useAuthContext } from '../../features/authentication';
+
+type Props = {
+  children: ReactNode;
+}
+
+export function StudentLayout({children}: Props ) {
+  const router = useRouter();
+  const [opened, setOpened] = useState(false);
+  const { classes } = useStyles();
+  const { userMe } = useAuthContext();
+
+  const getInitials = (nameString: string) => {
+    const fullName = nameString.split(' ');
+    const initials = fullName.shift()!.charAt(0) + fullName.pop()!.charAt(0);
+    return initials.toUpperCase();
+  }
+
+
+  const data = [
+    { 
+      label: 'Dashboard', 
+      icon: IconGauge, 
+      link: '/students', 
+      active: router.pathname === '/students' ? true : false
+    },
+    {
+      label: 'Live Session',
+      icon: IconUsers,
+      link: '/students/live-session',
+      active: router.pathname === '/students/live-session' ? true : false,
+    },
+    { 
+      label: 'Certificates', 
+      icon: IconCertificate, 
+      link: '/students/certificates', 
+      active: router.pathname === '/students/certificates' ? true : false,
+    },
+  
+    { 
+      label: 'Logout', 
+      icon: IconLogout, 
+      link: '/auth/sign-in',  
+      active: router.pathname === '/students/logout' ? true : false,
+    },
+  ];
+
+  const links = data.map((item) => <LinksGroup {...item} key={item.label}/>);
+
+  return (
+      <AppShell
+        header={
+         <Header height={60} p="md" style={{background: 'black'}}>
+          <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={`${colors.primaryColor}`}
+                mr="xl"
+              />
+            </MediaQuery>
+            <Box ml={30}>
+                <Text size={28} weight={600} color={'white'}>Coding4U</Text>
+            </Box>
+           
+          </div>
+        </Header>
+        }
+        navbar = {
+          <Navbar height={`cal(100vh - 50px)`} hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }} p="sm" className={classes.navbar} >
+            <Navbar.Section grow className={classes.links} component={ScrollArea}>
+                <div className={classes.linksInner}>{links}</div>
+            </Navbar.Section>
+
+            <Navbar.Section className={classes.footer}>
+              <UserButton
+                  initials={getInitials(`${userMe.firstName} ${userMe.lastName}`)}
+                  name={`${userMe.firstName} ${userMe.lastName}`}
+                  email={`${userMe.email}`}
+                />
+            </Navbar.Section>   
+        </Navbar>
+        }
+      >
+        {children}
+      </AppShell>
+    
+  );
+}

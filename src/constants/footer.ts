1081f9e9326624b14dce5b773b547274d3579693
maxwellwachira <maxwellwachira67@gmail.com>
2022-11-30 23:32:04 +0300

    added actual links

--- src/constants/footer.ts
+++ src/constants/footer.ts
@@ -12,18 +12,18 @@ export const footerData = [
         title: 'Courses', 
         links: [
             { id: 0, link: '/courses', label: 'all courses' },
-            { id: 1, link: '/courses:', label: 'Beginner' },
-            { id: 2, link: '/courses:', label: 'Intermediate' },
-            { id: 2, link: '/courses:', label: 'Advanced' },
+            { id: 1, link: '/courses/beginner', label: 'Beginner' },
+            { id: 2, link: '/courses/intermediate', label: 'Intermediate' },
+            { id: 2, link: '/courses/advanced', label: 'Advanced' },
         ]
     }, 
     {
         title: 'Contact', 
         links: [
             { id: 0, link: '/contact', label: 'Contact Page' },
-            { id: 1, link: 'mailto:', label: 'Support' },
-            { id: 2, link: 'mailto:', label: 'Info' },
-            { id: 2, link: 'mailto:', label: 'Sales' },
+            { id: 1, link: 'mailto:support@coding-4u.com', label: 'Support' },
+            { id: 2, link: 'mailto:inf0@coding-4u.com', label: 'Info' },
+            { id: 2, link: 'mailto:scholarship@coding-4u.com', label: 'Scholarship' },
         ]
     }, 
     {

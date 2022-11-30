7e4d7eeb01a5f4768096582fb630dad78d2f0e78
maxwellwachira <maxwellwachira67@gmail.com>
2022-11-30 12:12:13 +0300

    payment notification page

---
+++ src/pages/payment-notification.tsx
@@ -0,0 +1,114 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import {  Alert,  Center, Container,Paper, Stack } from '@mantine/core';
+import { useRouter } from 'next/router';
+import { IconCheck } from '@tabler/icons';
+
+import MainLayout from '../layouts/mainLayout/mainLayout';
+import { urls } from '../constants/urls';
+import { useAuthContext } from '../features/authentication';
+
+const PaymentNotification: NextPage = () => {
+   
+    const router = useRouter();
+    const { OrderTrackingId } = router.query;
+    const { userMe } = useAuthContext();
+    const [ response, setResponse ] = useState('') ;
+
+    const enroll = async(UserId: string, CourseId: string) => {
+        try {
+            const { status } = await axios.post(`${urls.baseUrl}/enrolment`, {UserId, CourseId});
+            if (status === 201){
+                return {
+                    message: "success"
+                };
+            }
+        } catch (error) {
+            console.log(error);
+            return {
+                message: "error"
+            };
+        }
+    }
+
+    const transactionStatus = async() => {
+        try {
+            const postData = {
+                orderTrackingId: OrderTrackingId
+            };
+            console.log(postData)
+            const { data } = await axios.post(`${urls.baseUrl}/pesapal/transaction-status`, postData );
+            if (data.status_code === 1){
+                switch (data.amount) {
+                    case 20000:
+                        const advanced = await enroll(userMe.id, '3');
+                        if (advanced?.message === "success"){
+                            setResponse("Payment was successful");
+                            setTimeout(() => {
+                                router.push('/students');
+                            }, 6000);
+                        }
+                        break;
+                    case 15000:
+                        const intermediate = await enroll(userMe.id, '2');
+                        if (intermediate?.message === "success"){
+                            setResponse("Payment was successful");
+                            setTimeout(() => {
+                                router.push('/students');
+                            }, 6000);
+                        }
+                        break;
+                    case 10000:
+                        const beginner = await enroll(userMe.id, '1');
+                        if (beginner?.message === "success"){
+                            setResponse("Payment was successful");
+                            setTimeout(() => {
+                                router.push('/students');
+                            }, 6000);
+                        }
+                        break;
+                
+                    default:
+                        setResponse("Error. Contact 254703519593 for further assistance");
+                        break;
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        transactionStatus(); 
+    }, []);
+
+    return (
+        <>
+        <Head>
+            <title>Coding4U | Payment Notification</title>
+            <meta name="description" content="Coding4U Payment Notification Page" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <MainLayout>
+            <Container>
+                <Stack justify="center" align="center" mt={30}>
+                    <Paper radius={40} withBorder style={{maxWidth: 400}}>
+                      <Center>
+                        {
+                            response === "" ? "" :
+                            <Alert icon={<IconCheck size={16} />} title="Payment status" color="green" radius={40} style={{maxWidth: 400}}>
+                                {response}
+                            </Alert>
+                        }                        
+                      </Center>
+                    </Paper>
+                </Stack>
+            </Container>
+        </MainLayout>
+        </>
+    );
+}
+
+export default PaymentNotification;

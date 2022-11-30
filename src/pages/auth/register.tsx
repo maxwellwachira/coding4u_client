3a390010edd2397eaf9d4b5e861e194274fc7dd1
maxwellwachira <maxwellwachira67@gmail.com>
2022-11-30 08:59:10 +0300

    added register user logic

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import {  Anchor, Button, Checkbox, Container, createStyles, Grid, Group, Paper, PasswordInput, Stack, Text, TextInput, } from '@mantine/core';
+import {  Anchor, Button, Checkbox, Container, createStyles, Notification, Divider, Grid, Group, Paper, PasswordInput, Stack, Text, TextInput, } from '@mantine/core';
 import { useViewportSize } from '@mantine/hooks';
+import { IconCheck, IconX } from '@tabler/icons';
 
 import MainLayout from '../../layouts/mainLayout/mainLayout';
 import { colors } from '../../constants/colors';
+import { useRegisterUser } from '../../features/authentication';
+
 
 
 const useStyles = createStyles((theme) => ({
@@ -40,7 +43,7 @@ const useStyles = createStyles((theme) => ({
         textAlign: "center",
         color:  theme.colors.gray[0],
         fontWeight: 600,
-        margin: '30px 0',
+        margin: '20px 0',
         boxShadow: '0 6px 10px 0 rgba(0,0,0,0.2)',
         '&:hover': {
            opacity: 0.7,
@@ -54,6 +57,7 @@ const useStyles = createStyles((theme) => ({
 const Register: NextPage = () => {
     const { classes } = useStyles();
     const { width } = useViewportSize();
+    const { form, response, loading, handleSubmit, clearResponse } = useRegisterUser();
 
     return (
         <>
@@ -78,7 +82,17 @@ const Register: NextPage = () => {
                             </Stack>
                         </Grid.Col>
                         <Grid.Col md={6} p={40}>
-                            <form>
+                            {response === 'success' ? (
+                                <Notification icon={<IconCheck size={18} />} color="teal" title="Registration Successful" onClose={clearResponse}>
+                                Check email to activate your account
+                                </Notification>
+                            ): response ? (
+                                <Notification icon={<IconX size={18} />} color="red" title="Error" onClose={clearResponse}>
+                                    {response}
+                                </Notification>
+                            ): ''}
+                            <Divider label="Register" labelPosition="center" my="lg" />
+                            <form onSubmit={form.onSubmit(() => handleSubmit())}>
                                 <Stack mt={20}>
                                     <Grid>
                                         <Grid.Col md = {6}>
@@ -87,6 +101,8 @@ const Register: NextPage = () => {
                                             label="First Name"
                                             placeholder="Your first name"
                                             radius={15}
+                                            value={form.values.firstName}
+                                            onChange={(event) => form.setFieldValue('firstName', event.currentTarget.value)}
                                         />
                                         </Grid.Col>
                                         <Grid.Col md={6}>
@@ -95,6 +111,8 @@ const Register: NextPage = () => {
                                             label="Last Name"
                                             placeholder="Your last name"
                                             radius={15}
+                                            value={form.values.lastName}
+                                            onChange={(event) => form.setFieldValue('lastName', event.currentTarget.value)}
                                         />
                                         </Grid.Col>
                                     </Grid>
@@ -104,6 +122,8 @@ const Register: NextPage = () => {
                                         label="Email"
                                         placeholder="hello@gmail.com"
                                         radius={15}
+                                        onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+                                        error={form.errors.email && 'Invalid email'}
                                     />
 
                                     <TextInput
@@ -111,6 +131,8 @@ const Register: NextPage = () => {
                                         label="Phone Number"
                                         placeholder="254703519593"
                                         radius={15}
+                                        value={form.values.phoneNumber}
+                                        onChange={(event) => form.setFieldValue('phoneNumber', event.currentTarget.value)}
                                     />
 
                                     <PasswordInput
@@ -118,24 +140,28 @@ const Register: NextPage = () => {
                                         label="Password"
                                         placeholder="Your password"
                                         radius={15}
+                                        onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+                                        error={form.errors.password && 'Password should include at least 6 characters'}
                                     />
 
 
                                     <Checkbox
                                         label="I accept terms and conditions"
                                         mt={15}
+                                        checked={form.values.terms}
+                                        onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
                                     />
                                 </Stack>
 
                                 <Group position="apart" mt="xl">
                                     <Anchor
-                                        href='/auth/sign-in'
+                                        href='/auth/login'
                                         color="dimmed"
                                         size="xs"
                                     >        
                                         Already have an account? Login
                                     </Anchor>
-                                    <Button type="submit" className={classes.submitButton} size="md">Register</Button>
+                                    <Button type="submit" className={classes.submitButton} size="md" loading={loading}>Register</Button>
                                 </Group>
                             </form>
                         </Grid.Col>

64d929c1944dba48f4784ca59f5973f5bbe07cf8
maxwellwachira <maxwellwachira67@gmail.com>
2023-02-27 13:11:29 +0300

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { ColorScheme, MantineProvider } from '@mantine/core';
 
 import '../styles/globals.css';
@@ -28,7 +29,21 @@ function MyApp({ Component, pageProps }: AppProps) {
           
             { !loading ?
               (<PageLoader />) :
-              (<Component {...pageProps} />)
+              <>
+              <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-VPNNPKHXZM" />
+
+              <Script strategy="lazyOnload">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-G-VPNNPKHXZM', {
+                  page_path: window.location.pathname,
+                  });
+              `}
+              </Script>
+              <Component {...pageProps} />
+            </>
             }
         </MantineProvider>
     </AuthContextProvider>
